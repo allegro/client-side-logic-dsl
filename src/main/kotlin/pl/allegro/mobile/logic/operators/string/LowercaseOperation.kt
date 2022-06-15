a281efa05052bac1fc2f7882f39d84e1ccbc0c0b
Rafal Kapela <rafal.kapela@allegro.pl>
2022-06-15 13:48:02 +0200

    uppercase operation

--- src/main/kotlin/pl/allegro/mobile/logic/operators/string/LowercaseOperation.kt
+++ src/main/kotlin/pl/allegro/mobile/logic/operators/string/LowercaseOperation.kt
@@ -1,25 +1,20 @@
 package pl.allegro.mobile.logic.operators.string
 
-import pl.allegro.mobile.logic.ClientLogic.isEmpty
-import pl.allegro.mobile.logic.ClientLogic.isEqual
-import pl.allegro.mobile.logic.ClientLogic.isNotEmpty
 import pl.allegro.mobile.logic.ClientLogicElement
 import pl.allegro.mobile.logic.ClientLogicMarker
-import pl.allegro.mobile.logic.ClientLogicOperator
-import pl.allegro.mobile.logic.ClientRegistryDataElement
-import pl.allegro.mobile.logic.NumberElement
 import pl.allegro.mobile.logic.operators.OperatorFactory
 
 internal interface LowercaseOperation {
     /**
      * Converts the character sequence to lower case.
+     * @receiver Character sequence or client side operation that returns string
      * @param sequence client side data or operation results which will be injected to string
      * @return lowercase operator, evaluated client side.
      * Operator returns a copy of this string converted to lower case.
      * @see: LowercaseOperationTest
      */
     @ClientLogicMarker
-    fun lowercase(sequence: ClientLogicElement) = LowercaseOperatorFactory().create(element)
+    fun lowercase(sequence: ClientLogicElement) = LowercaseOperatorFactory().create(sequence)
 
     @ClientLogicMarker
     fun ClientLogicElement.toLowercase() = LowercaseOperatorFactory().create(this)
