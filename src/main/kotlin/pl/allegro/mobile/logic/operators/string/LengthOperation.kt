244a17f8f78590b501110b71dab08b1830c77b37
Rafal Kapela <rafal.kapela@allegro.pl>
2022-06-15 13:28:13 +0200

    Docs correction

--- src/main/kotlin/pl/allegro/mobile/logic/operators/string/LengthOperation.kt
+++ src/main/kotlin/pl/allegro/mobile/logic/operators/string/LengthOperation.kt
@@ -1,16 +1,14 @@
 package pl.allegro.mobile.logic.operators.string
 
-import pl.allegro.mobile.logic.ClientLogicArray
 import pl.allegro.mobile.logic.ClientLogicElement
 import pl.allegro.mobile.logic.ClientLogicMarker
-import pl.allegro.mobile.logic.ClientLogicOperator
 import pl.allegro.mobile.logic.operators.OperatorFactory
 
 internal interface LengthOperation {
     /**
      * Returns the length of this character sequence.
-     * @receiver String or client side operation that returns integer
-     * @param sequence client side list or operation returning array
+     * @receiver Character sequence or client side operation that returns string
+     * @param sequence character sequence or operation returning string
      * @return length of the string evaluated client side.
      * Operator returns integer size
      * @see: SizeOperationTest
