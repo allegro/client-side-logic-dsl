e9764ee869b2395dcd18ddbc1bdc3579727eaacf
Rafal Kapela <rafal.kapela@allegro.pl>
2022-06-28 18:22:15 +0200

    Initial reverse operation dsl

---
+++ src/main/kotlin/pl/allegro/mobile/logic/operators/ReverseOperation.kt
@@ -0,0 +1,22 @@
+package pl.allegro.mobile.logic.operators
+
+import pl.allegro.mobile.logic.ClientLogicElement
+import pl.allegro.mobile.logic.ClientLogicMarker
+
+internal interface ReverseOperation {
+    /**
+     * Reverses elements in the array in-place.
+     * @receiver Character sequence, list of client side elements (use buildListOfElements or listOfElements) or client side operation that returns string or list.
+     * @param element string, client side list or operation returning array or character sequence
+     * @return reverse operator, evaluated client side.
+     * Operator returns this string or list of client side elements reversed.
+     * @see: ReverseOperationTest
+     */
+    @ClientLogicMarker
+    fun reverse(element: ClientLogicElement) = ReverseOperatorFactory().create(element)
+
+    @ClientLogicMarker
+    fun ClientLogicElement.reversed() = ReverseOperatorFactory().create(this)
+}
+
+private class ReverseOperatorFactory : OperatorFactory("uppercase")
