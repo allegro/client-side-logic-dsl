e7b03da72f4732c6b2b1edaea9625d40ed029c51
Rafal Kapela <rafal.kapela@allegro.pl>
2022-06-28 20:17:46 +0200

    clena up

--- src/test/kotlin/pl/allegro/mobile/logic/operators/array/DistinctOperationTest.kt
+++ src/test/kotlin/pl/allegro/mobile/logic/operators/array/DistinctOperationTest.kt
@@ -2,7 +2,6 @@ package pl.allegro.mobile.logic.operators.array
 
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import pl.allegro.mobile.logic.ClientLogic.distinct
 import pl.allegro.mobile.logic.clientLogic
 import pl.allegro.mobile.logic.operators.JsonLogicTestData
 import pl.allegro.mobile.logic.operators.assertSerializedExpressionMatchesExpected
