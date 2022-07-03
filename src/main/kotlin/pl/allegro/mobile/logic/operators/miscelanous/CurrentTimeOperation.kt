17f058c16ad295b7421143ebd9e92cdd7baa7f80
Rafal Kapela <rafal.kapela@allegro.pl>
2022-07-03 21:09:38 +0200

    documentation update

--- src/main/kotlin/pl/allegro/mobile/logic/operators/miscelanous/CurrentTimeOperation.kt
+++ src/main/kotlin/pl/allegro/mobile/logic/operators/miscelanous/CurrentTimeOperation.kt
@@ -4,6 +4,12 @@ import pl.allegro.mobile.logic.ClientLogicMarker
 import pl.allegro.mobile.logic.operators.OperatorFactory
 
 internal interface CurrentTimeOperation {
+    /**
+     * The difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC.
+     * @return currentTime operator, evaluated client side.
+     * Operator returns difference, measured in milliseconds, between current time and midnight January 1, 1970 UTC.
+     * @see: CurrentTimeOperationTest
+     */
     @ClientLogicMarker
     fun currentTimeInMillis() = CurrentTimeOperatorFactory().create()
 }
