de37fdac3c45ae814336809359525d1d163e070b
Kamil Strąk <kamil.strak@allegro.pl>
2023-03-30 15:05:56 +0200

    [MBOX-5038] - Compare to date: - PR improvements

--- src/main/kotlin/pl/allegro/mobile/logic/operators/string/CompareToDateOperation.kt
+++ src/main/kotlin/pl/allegro/mobile/logic/operators/string/CompareToDateOperation.kt
@@ -5,17 +5,16 @@ import pl.allegro.mobile.logic.ClientLogicMarker
 import pl.allegro.mobile.logic.ClientLogicOperator
 import pl.allegro.mobile.logic.StringElement
 
-
 internal interface  CompareToDateOperation {
     /**
-     * Compares a string to given string represents the date in proper format.
+     * Compares a string to given string representing the date in a proper format.
      * This operation compares the date
      * @receiver Character sequence or client side operation that returns string
      * @param date date to compare to date represents in current string
      * @return integer interpretation of state. State is between -1 and 1.
-     * -1 - passing date is BEFORE the compared date
-     * 0 - passing date is EQUALS the compared date
-     * 1 - passing date is AFTER the compared date
+     * 1 - passed date is BEFORE the compared date
+     * 0 - passed date is EQUALS the compared date
+     * -1 - passed date is AFTER the compared date
      */
     @ClientLogicMarker
     fun ClientLogicElement.compareToDate(date: String) =
