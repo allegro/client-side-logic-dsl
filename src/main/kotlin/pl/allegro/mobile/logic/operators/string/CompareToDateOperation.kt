41869e48780d1ab99d7127f1e3e7e19ce3a05487
Kamil Strąk <kamil.strak@allegro.pl>
2023-04-14 15:09:07 +0200

    [MBOX-5038] - Add compare precision

--- src/main/kotlin/pl/allegro/mobile/logic/operators/string/CompareToDateOperation.kt
+++ src/main/kotlin/pl/allegro/mobile/logic/operators/string/CompareToDateOperation.kt
@@ -11,45 +11,53 @@ internal interface  CompareToDateOperation {
      * This operation compares the date
      * @receiver Character sequence or client side operation that returns string
      * @param date date to compare to date represents in current string
+     * @param precision precision to compare dates
      * @return integer interpretation of state. State is between -1 and 1.
      * -1 - passed date is AFTER the compared date
      * 0 - passed date is EQUALS the compared date
      * 1 - passed date is BEFORE the compared date
      */
     @ClientLogicMarker
-    fun ClientLogicElement.compareToDate(date: String) =
-        CompareToDateOperationFactory().create(this,date)
+    fun ClientLogicElement.compareToDate(date: String, precision: ComparePrecision) =
+        CompareToDateOperationFactory().create(this, date, precision)
 
     /**
      * Compares a string to given string representing the date in a proper format.
      * This operation compares the date
      * @receiver Character sequence or client side operation that returns string
      * @param registryKey key represents stored date to compare
+     * @param precision precision to compare dates
      * @return integer interpretation of state. State is between -1 and 1.
      * -1 - passed date is AFTER the compared date
      * 0 - passed date is EQUALS the compared date
      * 1 - passed date is BEFORE the compared date
      */
     @ClientLogicMarker
-    fun ClientLogicElement.compareToDate(registryKey: ClientLogicElement) =
-        CompareToDateOperationFactory().create(this, registryKey)
+    fun ClientLogicElement.compareToDate(registryKey: ClientLogicElement, precision: ComparePrecision) =
+        CompareToDateOperationFactory().create(this, registryKey, precision)
 }
 
+enum class ComparePrecision {
+    MILLISECOND,SECOND,MINUTE,HOUR,DAY,MONTH,YEAR
+}
 private class CompareToDateOperationFactory {
     fun create(
         element: ClientLogicElement,
-        date: String
+        date: String,
+        precision: ComparePrecision
     ) = ClientLogicOperator.Builder("compareToDate")
         .add(element)
         .add(StringElement(date))
+        .add(StringElement(precision.name))
         .build()
 
     fun create(
         element: ClientLogicElement,
-        date: ClientLogicElement
+        date: ClientLogicElement,
+        precision: ComparePrecision
     ) = ClientLogicOperator.Builder("compareToDate")
         .add(element)
         .add(date)
+        .add(StringElement(precision.name))
         .build()
-
 }
--- src/test/kotlin/pl/allegro/mobile/logic/operators/string/CompareToDateOperationTest.kt
+++ src/test/kotlin/pl/allegro/mobile/logic/operators/string/CompareToDateOperationTest.kt
@@ -24,16 +24,100 @@ class CompareToDateOperationTest {
                 JsonLogicTestData(
                     testCase = "",
                     expression = clientLogic {
-                        registryKey("date").compareToDate("2022-02-01")
+                        registryKey("date").compareToDate("2022-02-01", ComparePrecision.MILLISECOND)
                     },
-                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01"]}"""
+                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01","MILLISECOND"]}"""
                 ),
                 JsonLogicTestData(
                     testCase = "",
                     expression = clientLogic {
-                        registryKey("date").compareToDate(registryKey("date1"))
+                        registryKey("date").compareToDate(registryKey("date1"), ComparePrecision.MILLISECOND)
                     },
-                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"}]}"""
+                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"},"MILLISECOND"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate("2022-02-01", ComparePrecision.SECOND)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01","SECOND"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate(registryKey("date1"), ComparePrecision.SECOND)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"},"SECOND"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate("2022-02-01", ComparePrecision.MINUTE)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01","MINUTE"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate(registryKey("date1"), ComparePrecision.MINUTE)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"},"MINUTE"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate("2022-02-01", ComparePrecision.HOUR)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01","HOUR"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate(registryKey("date1"), ComparePrecision.HOUR)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"},"HOUR"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate("2022-02-01", ComparePrecision.DAY)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01","DAY"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate(registryKey("date1"), ComparePrecision.DAY)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"},"DAY"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate("2022-02-01", ComparePrecision.MONTH)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01","MONTH"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate(registryKey("date1"), ComparePrecision.MONTH)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"},"MONTH"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate("2022-02-01", ComparePrecision.YEAR)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},"2022-02-01","YEAR"]}"""
+                ),
+                JsonLogicTestData(
+                    testCase = "",
+                    expression = clientLogic {
+                        registryKey("date").compareToDate(registryKey("date1"), ComparePrecision.YEAR)
+                    },
+                    expected = """{"compareToDate": [{"var": "date"},{"var": "date1"},"YEAR"]}"""
                 ),
             ).toJsonLogicTestArgumentsStream()
         }
